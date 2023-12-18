aae7bd9ab666879a6bd686f1f577a5c667e767cf
Venukanti-Mamatha <153125955+Venukanti-Mamatha@users.noreply.github.com>
2023-12-18 21:25:10 +0530

    Create index.js

---
+++ http-server/index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require("fs");
+
+let homeContent = "";
+let projectContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+fs.readFile("registration.html",(err,registration)=>{
+  if(err){
+    throw err;
+  }
+  registrationContent = registration;
+});
